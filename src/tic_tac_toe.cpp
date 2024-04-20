5cf8516aad23fa3ec271167872649586d8b85e21
yadokani389 <yadokani389@gmail.com>
2024-04-20 12:29:07 +0900

    Update tic-tac-toe game logic and UI components

--- src/tic_tac_toe.cpp
+++ src/tic_tac_toe.cpp
@@ -49,51 +49,47 @@ void tic_tac_toe_init(dpp::cluster &bot) {
       size_t y = event.custom_id[event.custom_id.size() - 1] - '0';
       size_t x = event.custom_id[event.custom_id.size() - 2] - '0';
 
-      for (size_t i = 0; i < 3; i++) {
-        dpp::component cmp;
-        for (size_t j = 0; j < 3; j++) {
+      for (size_t i = 0; i < 3; i++)
+        for (size_t j = 0; j < 3; j++)
           if (x == i && y == j)
             field[j + i * 3] = pre_turn;
-          cmp.add_component(
-              dpp::component()
-                  .set_label(std::string(1, field[j + i * 3]))
-                  .set_style(dpp::cos_primary)
-                  .set_disabled(field[j + i * 3])
-                  .set_id("tic-tac-toe" + std::string(1, i + '0') + std::string(1, j + '0')));
-        }
-        msg.add_component(cmp);
-      }
 
+      bool end = false;
       {
         char winner = '\0';
         for (int i = 0; i < 3; i++) {
-          if (field[0 + i * 3] == field[1 + i * 3] && field[1 + i * 3] == field[2 + i * 3])
+          if (field[0 + i * 3] == field[1 + i * 3] && field[1 + i * 3] == field[2 + i * 3] && field[0 + i * 3])
             winner = field[0 + i * 3];
-          if (field[i + 0 * 3] == field[i + 1 * 3] && field[i + 1 * 3] == field[i + 2 * 3])
+          if (field[i + 0 * 3] == field[i + 1 * 3] && field[i + 1 * 3] == field[i + 2 * 3] && field[i + 0 * 3])
             winner = field[i + 0 * 3];
         }
-        if (field[0 + 0 * 3] == field[1 + 1 * 3] && field[1 + 1 * 3] == field[2 + 2 * 3])
+        if (field[0 + 0 * 3] == field[1 + 1 * 3] && field[1 + 1 * 3] == field[2 + 2 * 3] && field[0 + 0 * 3])
           winner = field[0 + 0 * 3];
-        if (field[2 + 0 * 3] == field[1 + 1 * 3] && field[1 + 1 * 3] == field[0 + 2 * 3])
+        if (field[2 + 0 * 3] == field[1 + 1 * 3] && field[1 + 1 * 3] == field[0 + 2 * 3] && field[2 + 0 * 3])
           winner = field[2 + 0 * 3];
 
         if (winner) {
           msg.content = std::string(1, winner) + " wins!";
-          bot.message_edit_sync(msg);
-          event.reply();
-          field.fill('\0');
-
-          return;
+          end = true;
         }
       }
 
       if (std::count(field.begin(), field.end(), '\0') == 0) {
         msg.content = "draw!";
-        bot.message_edit_sync(msg);
-        event.reply();
-        field.fill('\0');
+        end = true;
+      }
 
-        return;
+      for (size_t i = 0; i < 3; i++) {
+        dpp::component cmp;
+        for (size_t j = 0; j < 3; j++) {
+          cmp.add_component(
+              dpp::component()
+                  .set_label(std::string(1, field[j + i * 3]))
+                  .set_style(dpp::cos_primary)
+                  .set_disabled(field[j + i * 3] || end)
+                  .set_id("tic-tac-toe" + std::string(1, i + '0') + std::string(1, j + '0')));
+        }
+        msg.add_component(cmp);
       }
 
       bot.message_edit_sync(msg);
